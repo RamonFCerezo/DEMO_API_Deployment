494b9ff6693841a6f8974addce23156603fc8556
RamonFCerezo <117541707+RamonFCerezo@users.noreply.github.com>
2023-02-28 18:29:25 +0100

    methods2

--- app_model_db.py
+++ app_model_db.py
@@ -33,9 +33,9 @@ def predict():
 
 # 2. Un endpoint para almacenar nuevos registros en la base de datos que deberá estar previamente creada.
 
-@app.route('/v1/ingest_data', methods=['GET, POST'])
+@app.route('/v1/ingest_data', methods=['POST', 'GET'])
 def new_data():
-    if 'tv' in request.args and 'radio' in request.args and 'newspaper' in request.args and 'sales' in request.args:
+    if request.method == 'POST'and 'tv' in request.args and 'radio' in request.args and 'newspaper' in request.args and 'sales' in request.args:
         connection = sqlite3.connect('vent.db')
         crsr = connection.cursor()
         radio = float(request.args['radio'])
@@ -55,23 +55,24 @@ def new_data():
 
 # 3. Posibilidad de reentrenar de nuevo el modelo con los posibles nuevos registros que se recojan.
 
-@app.route('/v2/retrain', methods=['GET, POST'])
+@app.route('/v2/retrain', methods=['POST', 'GET'])
 
 def retrain():
-    model = pickle.load(open('advertising_model','rb'))
-    connection = sqlite3.connect("vent.db")
-    crsr = connection.cursor()
-    query = '''SELECT * FROM VENTAS'''
-    crsr.execute(query)
-    ans = crsr.fetchall()
-    names = [description[0] for description in crsr.description]
-    X = pd.DataFrame(ans,columns=names)
-    X_train = X[['TV', 'radio', 'newspaper']]
-    y_train = X['sales']
-    model.fit(X_train, y_train)
-    with open("advertising_model", "wb") as f:
-        pickle.dump(model, f)
-    connection.close()
-    return "<b>Model updated with new data<b>"
+    if request.method == 'POST':
+        model = pickle.load(open('advertising_model','rb'))
+        connection = sqlite3.connect("vent.db")
+        crsr = connection.cursor()
+        query = '''SELECT * FROM VENTAS'''
+        crsr.execute(query)
+        ans = crsr.fetchall()
+        names = [description[0] for description in crsr.description]
+        X = pd.DataFrame(ans,columns=names)
+        X_train = X[['TV', 'radio', 'newspaper']]
+        y_train = X['sales']
+        model.fit(X_train, y_train)
+        with open("advertising_model", "wb") as f:
+            pickle.dump(model, f)
+        connection.close()
+        return "<b>Model updated with new data<b>"
 
 # app.run()
